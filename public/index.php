06d253097cac6793290b8c780c9b7dd174e1927e
Joe Alamo <joe.keilty+github@gmail.com>
2016-07-01 16:37:48 +0100

    Add Composer for packages & autoloading

--- public/index.php
+++ public/index.php
@@ -7,22 +7,9 @@
  */
 
 /**
- * Twig
+ * Composer
  */
-require_once dirname(__DIR__) . '/vendor/Twig/Autoloader.php';
-Twig_Autoloader::register();
-
-/**
- * Autoloader
- */
-
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $file;
-    }
-});
+require '../vendor/autoload.php';
 
 $router = new \Core\Routing\Router();
 
